3f236f796f22e1265dd37e7eed5c92ad29f26a8f
PsychoDela <farisdelic.fd@gmail.com>
2020-04-20 16:52:22 +0200

    First commit

---
+++ Main.java
@@ -0,0 +1,39 @@
+import java.util.ArrayList;
+
+public class Main 
+{
+	public static void main(String[] args)
+	{
+		
+	}
+	
+	public static int stringLength(String string)
+	{
+		return 0;
+	}
+	
+	public static ArrayList <String> charsOnEvenNums(String string)
+	{
+		return null;
+	}
+	
+	public static ArrayList <String> charsOnOddNums(String string)
+	{
+		return null;
+	}
+	
+	public static int numOfUppercaseChars(String string)
+	{
+		return 0;
+	}
+	
+	public static int numOfLowercaseChars(String string)
+	{
+		return 0;
+	}
+	
+	public static ArrayList <String> charsWhichNotLetters(String string)
+	{
+		return null;
+	}
+}
