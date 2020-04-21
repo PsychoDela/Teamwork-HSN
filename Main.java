a9eac1cb4e54b7fba4fa9b6904b447b68f39f8eb
merfid637 <alihodzicmerfid0@gmail.com>
2020-04-21 16:26:29 +0200

    Second Commit

--- Main.java
+++ Main.java
@@ -1,39 +1,36 @@
 import java.util.ArrayList;
+import java.util.Scanner;
 
-public class Main 
-{
-	public static void main(String[] args)
-	{
-		
+public class Main {
+
+	public static void main(String[] args) {
 	}
-	
-	public static int stringLength(String string)
-	{
-		return 0;
+
+	public static int stringLength(String string) {
+
+		if (string.length() == 0) {
+			return 0;
+		} else
+			return string.length();
 	}
-	
-	public static ArrayList <String> charsOnEvenNums(String string)
-	{
+
+	public static ArrayList<String> charsOnEvenNums(String string) {
 		return null;
 	}
-	
-	public static ArrayList <String> charsOnOddNums(String string)
-	{
+
+	public static ArrayList<String> charsOnOddNums(String string) {
 		return null;
 	}
-	
-	public static int numOfUppercaseChars(String string)
-	{
+
+	public static int numOfUppercaseChars(String string) {
 		return 0;
 	}
-	
-	public static int numOfLowercaseChars(String string)
-	{
+
+	public static int numOfLowercaseChars(String string) {
 		return 0;
 	}
-	
-	public static ArrayList <String> charsWhichNotLetters(String string)
-	{
+
+	public static ArrayList<String> charsWhichNotLetters(String string) {
 		return null;
 	}
 }
